50b5917d981a447d6fea361661429cbdbb0b964d
Roland RAKOTOMALALA <rolrkt@yahoo.com>
2025-09-16 18:27:02 +0200

    readme push de fin de séance

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.defaultHome, name="home"), # "" = route par défaut
+    path("home/", views.home, name="home"),
     path("home/<param>/", views.home, name="home"),
     path("contact/", views.contact, name="contact"),
-    path("aboutus/", views.aboutus, name="aboutus"),
+    path("aboutus/", views.about, name="aboutus"),
     path("produits/", views.ListProduits, name="listproduits"),
     path("categories/", views.ListCategories, name="listcategories"),
     path("status/", views.ListStatuts, name="liststatus"),
+    path('ma_vue/', views.ma_vue, name="ma_vuuue")
 ]
 
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -3,28 +3,32 @@ from django.http import HttpResponse
 from .models import Produit
 from .models import Categorie
 from .models import Statut
+from django.http import HttpResponse, Http404, JsonResponse
 
 # Create your views here.
-def defaultHome(request):
-    return HttpResponse("<h1>Bonjour inconnu !</h1>")
 
+def home(request,param=None):
 
-def home(request, param):
-    return HttpResponse(f"<h1>Salut {param}</h1>")
-
+    if request.GET and request.GET["test"]:
+        raise Http404
+    
+    if param is None:
+        print(dir(request))
+        print(request.__dict__)
+        string = request.GET['name']
+        return HttpResponse("Bonjour %s!" % string)
+    else:
+        return HttpResponse(f"<h1>Hello {param} !</h1>")
+    
+def about(request):
+    return HttpResponse("<p> Bienvenu sur la page A propos !</p>")
+    
 def contact(request):
-    return HttpResponse("<h1>Contactez-nous</h1>")
-
-def aboutus(request):
-    return HttpResponse("<h1>À propos de nous</h1>")
+    return HttpResponse("<p> Bienvenu sur la page de contact !</p>")
 
 def ListProduits(request):
-    prds = Produit.objects.all()
-    lesProduits = "<ul>"
-    for p in prds:
-        lesProduits = lesProduits + "<li>" + p.intituleProd + "</li>"
-    lesProduits = lesProduits + "</ul>"
-    return HttpResponse("<h1>Liste des produits</h1>" + lesProduits)
+    prdts = Produit.objects.all()
+    return render(request, 'monApp/list_produits.html',{'prdts': prdts})
 
 def ListCategories(request):
     cats = Categorie.objects.all()
@@ -41,4 +45,7 @@ def ListStatuts(request):
         lesStatus = lesStatus + "<li>" + s.libelle + "</li>"
     lesStatus = lesStatus + "</ul>"
     return HttpResponse("<h1>Liste des catégories</h1>" + lesStatus)
-    
+
+def ma_vue(request):
+    return JsonResponse({'foo': 'bar'})
+
